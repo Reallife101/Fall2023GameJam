fce5a19970df6c388d79545e8d70acd92b45c9b4
coleyoung02 <coleyoung741@gmail.com>
2023-10-27 20:04:16 -0700

    working

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
@@ -5,19 +6,59 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    Vector3 mousePos;
-    [SerializeField] Camera cam;
+    private Vector3 mousePos;
+    [SerializeField] private Camera cam;
+    [SerializeField] private Bullet bullet;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float fireDelay;
+    [SerializeField] private float reloadDelay;
+    [SerializeField] private float loadSize;
+    private float fClock;
+    private float rClock;
+    private float inClip;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        fClock = 0;
+        rClock = 0;
+        inClip = loadSize;
     }
 
     // Update is called once per frame
     void Update()
     {
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 direction = mousePos - gameObject.transform.position;
-        direction = direction.normalized;
+        Vector3 mousePos = FindObjectOfType<Camera>().ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = mousePos - FindObjectOfType<Gun>().gameObject.transform.position;
+        Vector2 velocity = (new Vector2(direction.x, direction.y)).normalized;
+        transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg);
+        
+
+        if (Input.GetMouseButton(0) && fClock <= 0 && inClip > 0 && rClock <= 0)
+        {
+            shoot();
+            fClock = fireDelay;
+            inClip -= 1;
+            if (inClip == 0)
+            {
+                reload();
+            }
+        }
+
+        fClock -= Time.deltaTime;
+        rClock -= Time.deltaTime;
+    }
+
+    private void shoot()
+    {
+        Instantiate(bullet, transform);
+    }
+
+    private void reload()
+    {
+        inClip = loadSize;
+        rClock = reloadDelay;
     }
 }
