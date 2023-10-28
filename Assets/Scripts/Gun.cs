4cff15b903526ab546447b5ad0c8071dd2105c45
coleyoung02 <coleyoung741@gmail.com>
2023-10-28 13:14:28 -0700

    no tmPro

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -12,10 +12,11 @@ public class Gun : MonoBehaviour
     [SerializeField] private float speed = 5f;
     [SerializeField] private float fireDelay;
     [SerializeField] private float reloadDelay;
-    [SerializeField] private float loadSize;
+    [SerializeField] private int loadSize;
+    [SerializeField] private float coneSize;
     private float fClock;
     private float rClock;
-    private float inClip;
+    private int inLoad;
 
 
 
@@ -24,28 +25,32 @@ public class Gun : MonoBehaviour
     {
         fClock = 0;
         rClock = 0;
-        inClip = loadSize;
+        inLoad = loadSize;
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 mousePos = FindObjectOfType<Camera>().ScreenToWorldPoint(Input.mousePosition);
-        Vector3 direction = mousePos - FindObjectOfType<Gun>().gameObject.transform.position;
+        Vector3 direction = mousePos - gameObject.transform.position;
         Vector2 velocity = (new Vector2(direction.x, direction.y)).normalized;
-        transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg);
-        
 
-        if (Input.GetMouseButton(0) && fClock <= 0 && inClip > 0 && rClock <= 0)
+        
+        if (Input.GetKeyDown(KeyCode.R)) 
+        {
+            reload();
+        }
+        if (Input.GetMouseButton(0) && fClock <= 0 && inLoad > 0 && rClock <= 0)
         {
             shoot();
-            fClock = fireDelay;
-            inClip -= 1;
-            if (inClip == 0)
+            
+            if (inLoad == 0)
             {
                 reload();
             }
         }
+        Aim(velocity, fClock <= 0 || rClock > 0);
+
 
         fClock -= Time.deltaTime;
         rClock -= Time.deltaTime;
@@ -53,12 +58,40 @@ public class Gun : MonoBehaviour
 
     private void shoot()
     {
+        transform.rotation = Quaternion.AngleAxis(UnityEngine.Random.Range(-coneSize / 2f, coneSize / 2f), new Vector3(0, 0, 1)) * transform.rotation;
         Instantiate(bullet, transform.position, Quaternion.identity);
+        fClock = fireDelay;
+        inLoad -= 1;
+        FindObjectOfType<GunUI>().updateUI(inLoad);
+    }
+
+    private void Aim(Vector2 dir, bool complete)
+    {
+        Debug.Log(complete);
+        if (!complete)
+        {
+            transform.rotation = Quaternion.Lerp(Quaternion.Euler(0, 0, (Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg)), transform.rotation, .75f * Mathf.Pow(0.5f, Time.deltaTime));
+        }
+        else 
+        {
+            transform.rotation = Quaternion.Euler(0, 0, (Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg));
+        }
     }
 
     private void reload()
     {
-        inClip = loadSize;
+        inLoad = loadSize;
         rClock = reloadDelay;
+        FindObjectOfType<GunUI>().reload(reloadDelay);
+    }
+
+    public int getBullets()
+    {
+        return inLoad;
+    }
+
+    public int getMax()
+    {
+        return loadSize;
     }
 }
