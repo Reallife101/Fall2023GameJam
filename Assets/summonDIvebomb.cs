01ac6e9df04b43130df56980b5915178bea4f5a5
Ryan Ngoon <ryan.ngoon@gmail.com>
2023-10-28 16:00:03 -0700

    finish divebomb enemies

--- Assets/summonDIvebomb.cs
+++ Assets/summonDIvebomb.cs
@@ -2,17 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class summonDIvebomb : MonoBehaviour
+public class summonDiveBomb : Attacks
 {
+    private GameObject[] spawns;
+    [SerializeField]
+    int waves;
+    [SerializeField]
+    private GameObject enemy;
+    private AI ai;
+
+    public override void atk()
+    {
+        coroutine = waitEnable();
+        StartCoroutine(coroutine);
+    }
+
+    public override void stopAtk()
+    {
+        StopCoroutine(coroutine);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawns = GameObject.FindGameObjectsWithTag("Spawn");
+        ai = GetComponent<AI>();
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator waitEnable()
     {
-        
+        for (int i = 0; i < waves; i++)
+        {
+            yield return new WaitForSeconds(1);
+            Instantiate(enemy, spawns[Random.Range(0, spawns.Length)].transform.position, Quaternion.identity);
+
+        }
+
+
+        ai.canAttack = true;
     }
 }
