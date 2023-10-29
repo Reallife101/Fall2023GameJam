3c594dac472a3056aabd5ca2914705997fca3721
Reallife101 <ryan.ngoon@gmail.com>
2023-10-28 21:29:37 -0700

    fix driveby

--- Assets/summonDriveby.cs
+++ Assets/summonDriveby.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class summonDiveBomb : Attacks
+public class summonDriveby : Attacks
 {
     private GameObject[] spawns;
     [SerializeField]
