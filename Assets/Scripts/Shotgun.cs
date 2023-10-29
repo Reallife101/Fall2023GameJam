6a2fbf6b6722bcfba2f844e3cb3470ea4082f675
Ryan Ngoon <ryan.ngoon@gmail.com>
2023-10-29 14:51:31 -0700

    add shotgun changes

--- Assets/Scripts/Shotgun.cs
+++ Assets/Scripts/Shotgun.cs
@@ -9,9 +9,9 @@ public class Shotgun : Gun
     protected override void shoot()
     {
         Quaternion q;
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 7; i++)
         {
-            q = Quaternion.AngleAxis((coneSize/5) * i -coneSize / 2f + UnityEngine.Random.Range(-coneSize / 8f, coneSize / 8f), new Vector3(0, 0, 1)) * transform.rotation;
+            q = Quaternion.AngleAxis((coneSize/7) * i -coneSize / 2f + UnityEngine.Random.Range(-coneSize / 8f, coneSize / 8f), new Vector3(0, 0, 1)) * transform.rotation;
             Instantiate(bullet, bulletSpot.transform.position, q);
         }
         CinemachineShake.Instance.ShakeCamera(shakeIntensity, shakeTime);
